a7e78cc0298dac8add3fae79fc292d30a89353b8
Fransnico Susanto <fransnico@amazeworx.com>
2023-10-18 09:20:40 +0700

    what's on

--- template-parts/sections/whats_on.php
+++ template-parts/sections/whats_on.php
@@ -12,6 +12,7 @@ $heading = $whats_on['heading'];
 $description = $whats_on['description'];
 $post_settings = $whats_on['post_settings'];
 $categories = $post_settings['categories'];
+$grid_style = $post_settings['grid_style'];
 $posts_per_page = $post_settings['posts_per_page'];
 if (!$posts_per_page) {
   $posts_per_page = '-1';
@@ -19,15 +20,35 @@ if (!$posts_per_page) {
 $primary_color = get_field('primary_color', 'option');
 $link_style = 'color: ' . $primary_color;
 
-$whats_on = new WP_Query(
-  array(
-    'post_type'         => 'whats-on',
-    'post_status '      => 'publish',
-    // 'orderby'           => 'menu_order',
-    // 'order'             => 'ASC',
-    'posts_per_page'    => $posts_per_page,
-  )
-);
+if (!$categories) {
+  $whats_on = new WP_Query(
+    array(
+      'post_type'         => 'whats-on',
+      'post_status '      => 'publish',
+      // 'orderby'           => 'menu_order',
+      // 'order'             => 'ASC',
+      'posts_per_page'    => $posts_per_page,
+    )
+  );
+} else {
+  $whats_on = new WP_Query(
+    array(
+      'post_type'         => 'whats-on',
+      'post_status '      => 'publish',
+      // 'orderby'           => 'menu_order',
+      // 'order'             => 'ASC',
+      'posts_per_page'    => $posts_per_page,
+      'tax_query' => array(
+        array(
+          'taxonomy' => 'whats-on-category',
+          'field'    => 'term_id',
+          'terms'    => $categories,
+        ),
+      ),
+    )
+  );
+}
+
 
 //preint_r($whats_on);
 
@@ -48,28 +69,60 @@ $whats_on = new WP_Query(
     <?php endif; ?>
 
     <?php if ($whats_on->have_posts()) : ?>
-      <div class="grid grid-cols-3 gap-x-10 mt-8">
-        <?php while ($whats_on->have_posts()) : $whats_on->the_post(); ?>
-          <?php
-          $id = get_the_ID();
-          ?>
-          <div>
-            <div class="aspect-w-1 aspect-h-1 rounded-lg mb-4">
+      <?php if ($grid_style == 'banner') : ?>
+        <div class="grid grid-cols-1 gap-y-10">
+          <?php while ($whats_on->have_posts()) : $whats_on->the_post(); ?>
+            <?php
+            $id = get_the_ID();
+            ?>
+            <div class="rounded-xl overflow-hidden">
               <?php if (has_post_thumbnail($id)) : ?>
-                <a href="<?php the_permalink() ?>" class="block">
-                  <?php echo get_the_post_thumbnail($id, 'full', array('class' => 'w-full h-full object-cover rounded-lg')); ?>
-                </a>
+                <?php echo get_the_post_thumbnail($id, 'full', array('class' => 'rounded-xl')); ?>
               <?php endif; ?>
             </div>
-            <h3 class="text-3xl font-normal mb-4"><a href="<?php the_permalink() ?>" class="text-slate-600 hover:underline"><?php the_title() ?></a></h3>
-            <div class="text-slate-500 text-sm"><?php the_excerpt() ?></div>
-            <div class="mt-4">
-              <a href="<?php the_permalink() ?>" class="text-sm uppercase font-medium hover:underline" style="<?php echo $link_style; ?>">FIND OUT MORE</a>
-            </div>
-          </div>
-        <?php endwhile; ?>
-      </div>
+          <?php endwhile; ?>
+        </div>
+      <?php else : ?>
+        <div class="grid grid-cols-3 gap-x-10 mt-8">
+          <?php while ($whats_on->have_posts()) : $whats_on->the_post(); ?>
+            <?php
+            $id = get_the_ID();
+            if ($grid_style == 'simple') :
+            ?>
+              <div>
+                <div class="aspect-w-1 aspect-h-1 rounded-lg mb-4">
+                  <?php if (has_post_thumbnail($id)) : ?>
+                    <a href="<?php the_permalink() ?>" class="block">
+                      <?php echo get_the_post_thumbnail($id, 'full', array('class' => 'w-full h-full object-cover rounded-lg')); ?>
+                    </a>
+                  <?php endif; ?>
+                </div>
+                <h3 class="text-3xl font-normal mb-4"><a href="<?php the_permalink() ?>" class="text-slate-600 hover:underline"><?php the_title() ?></a></h3>
+                <div class="text-slate-500 text-sm"><?php the_excerpt() ?></div>
+                <div class="mt-4">
+                  <a href="<?php the_permalink() ?>" class="text-sm uppercase font-medium hover:underline" style="<?php echo $link_style; ?>">FIND OUT MORE</a>
+                </div>
+              </div>
+            <?php elseif ($grid_style == 'card') : ?>
+              <div class="shadow-[0_3px_6px_rgba(0,0,0,0.16)] rounded-xl overflow-hidden">
+                <div class="aspect-w-16 aspect-h-10 rounded-t-xl">
+                  <?php if (has_post_thumbnail($id)) : ?>
+                    <?php echo get_the_post_thumbnail($id, 'full', array('class' => 'w-full h-full object-cover rounded-t-xl')); ?>
+                  <?php endif; ?>
+                </div>
+                <div class="p-6">
+                  <h3 class="text-xl font-bold mb-4"><?php the_title() ?></h3>
+                  <div class="text-slate-700 text-base"><?php the_content() ?></div>
+                </div>
+              </div>
+            <?php else : ?>
+            <?php endif; ?>
+          <?php endwhile; ?>
+        </div>
+      <?php endif; ?>
     <?php endif; ?>
+
+    <?php wp_reset_postdata(); ?>
 
   </div>
 </section>
