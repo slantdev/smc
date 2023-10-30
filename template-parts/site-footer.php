01a061d3459dcfdf58fb4ed34f5f832c6bf8ed5f
Fransnico Susanto <fransnico@amazeworx.com>
2023-10-30 09:01:32 +0700

    disable subscribe and instagram settings

--- template-parts/site-footer.php
+++ template-parts/site-footer.php
@@ -1,4 +1,7 @@
 <?php
+/*
+ * Footer Settings
+ */
 $site_logo = get_field('main_navigation', 'option')['main_navigation']['site_logo'];
 
 $footer_colors = get_field('footer_colors', 'option');
@@ -41,7 +44,77 @@ if ($copyright_text_color) {
   $copyright_style .= ' color: ' . $copyright_text_color . ';';
 }
 
+$subscribe = get_field('subscribe', 'option')['subscribe'];
+$subscribe_heading = $subscribe['heading'];
+$subscribe_description = $subscribe['description'];
+$subscribe_form_shortcode = $subscribe['form_shortcode'];
+$subscribe_background_color = $subscribe['subscribe_colors']['background_color'];
+$subscribe_text_color = $subscribe['subscribe_colors']['text_color'];
+$subscribe_style = '';
+if ($subscribe_background_color) {
+  $subscribe_style .= ' background-color: ' . $subscribe_background_color . ';';
+}
+if ($subscribe_text_color) {
+  $subscribe_style .= ' color: ' . $subscribe_text_color . ';';
+}
+
 ?>
+
+<?php
+wp_reset_query();
+
+$term_id = get_queried_object()->term_id;
+if ($term_id) {
+  $the_id = 'term_' . $term_id;
+} else {
+  $the_id = get_the_ID();
+}
+//echo $the_id;
+$disable_subscribe = get_field('disable_subscribe', $the_id);
+$disable_instagram = get_field('disable_instagram', $the_id);
+?>
+
+<?php if ($subscribe && !$disable_subscribe) : ?>
+  <section id="subscribe" style="<?php echo $subscribe_style ?>">
+    <div class="relative container max-w-screen-xl mx-auto pt-8 lg:pt-12 xl:pt-20 pb-8 lg:pb-12 xl:pb-20">
+      <div class="flex flex-col gap-y-6 lg:gap-y-0 lg:flex-row lg:gap-x-12">
+        <div class="w-full lg:w-1/2">
+          <?php if ($site_logo['logo_white']) : ?>
+            <div class="border-b border-white pb-6 mb-6 -mt-4">
+              <img src="<?php echo $site_logo['logo_white']['url'] ?>" alt="<?php echo get_bloginfo('name') ?>" class="w-auto h-6 lg:h-auto 2xl:h-14 max-w-full">
+            </div>
+          <?php endif; ?>
+          <div>
+            <?php if ($subscribe_heading) : ?>
+              <h3 class="text-3xl lg:text-4xl font-bold mb-4"><?php echo $subscribe_heading ?></h3>
+            <?php endif; ?>
+            <?php if ($subscribe_description) : ?>
+              <div><?php echo $subscribe_description ?></div>
+            <?php endif; ?>
+          </div>
+        </div>
+        <div class="w-full lg:w-1/2">
+          <div class="subscribe-form">
+            <?php if ($subscribe_form_shortcode) : ?>
+              <?php echo do_shortcode($subscribe_form_shortcode) ?>
+            <?php else : ?>
+              <div class="flex flex-col gap-8 w-full">
+                <div class="flex gap-4">
+                  <input type="text" class="bg-white rounded-full border-none w-full" placeholder="First name*">
+                  <input type="text" class="bg-white rounded-full border-none w-full" placeholder="Last name*">
+                </div>
+                <div><input type="text" class="bg-white rounded-full border-none w-full" placeholder="Email*"></div>
+                <div>
+                  <button type="button" class="bg-black py-3 px-12 rounded-full font-bold text-white hover:bg-opacity-60">Subscribe</button>
+                </div>
+              </div>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+<?php endif; ?>
 
 <footer style="<?php echo $footer_style ?>">
 
