563c30628cf57e2fd3166ce5c962698dcb3cde24
Slant Dev <build@amazeworx.com>
2024-02-29 08:51:13 +0700

    add classes to page banner

--- template-parts/layouts/page-header.php
+++ template-parts/layouts/page-header.php
@@ -46,19 +46,19 @@ if ($enabled) :
 
   $enable_breadcrumbs = get_field('enable_breadcrumbs');
 ?>
-  <section class="relative" style="<?php echo $hero_bg_style; ?>">
+  <section class="page-banner relative" style="<?php echo $hero_bg_style; ?>">
     <?php if ($hero_background) { ?>
       <div class="absolute inset-0 z-0">
         <?php if ($hero_overlay_color) { ?>
-          <div class="absolute inset-0 mix-blend-multiply" style="background-color: <?php echo $hero_overlay_color ?>"></div>
+          <div class="banner-bg-overlay absolute inset-0 mix-blend-multiply" style="background-color: <?php echo $hero_overlay_color ?>"></div>
         <?php } ?>
-        <img src="<?php echo $hero_background ?>" alt="" class="object-cover h-full w-full">
+        <img src="<?php echo $hero_background ?>" alt="" class="banner-bg-image object-cover h-full w-full">
       </div>
     <?php } ?>
     <div class="container max-w-screen-xl relative z-10 pt-52 lg:pt-[200px] 2xl:pt-[158px] h-full">
       <div class="flex flex-col md:flex-row h-full items-center text-white py-16 lg:py-24">
         <div class="w-full lg:w-1/2">
-          <h1 class="font-bold text-[40px] lg:text-[50px] leading-tight"><?php echo $hero_title ?></h1>
+          <h1 class="banner-title font-bold text-[40px] lg:text-[50px] leading-tight"><?php echo $hero_title ?></h1>
         </div>
       </div>
     </div>
