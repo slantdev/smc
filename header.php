11deac225095756f53ebc5a11a95d8ea77438e71
Slant Dev <build@amazeworx.com>
2026-04-06 01:51:11 +0700

    refactor: remove compiled webpack boilerplate from theme assets

--- header.php
+++ header.php
@@ -24,4 +24,21 @@
 
       <?php do_action('smc_content_start'); ?>
 
-      <main>
+      <?php
+      $queried_obj = get_queried_object();
+      $term_id = isset($queried_obj->term_id) ? $queried_obj->term_id : false;
+      if ($term_id) {
+        $the_id = 'term_' . $term_id;
+      } else {
+        $the_id = get_the_ID();
+      }
+      
+      $enable_page_header = function_exists('get_field') ? get_field('enable_page_header', $the_id) : false;
+      $main_classes = '';
+      
+      // If there is no page header, add padding to push content down below the fixed site header
+      if (!$enable_page_header) {
+        $main_classes = 'pt-[64px] lg:pt-[114px] xl:pt-[142px] 2xl:pt-[158px]';
+      }
+      ?>
+      <main class="<?php echo esc_attr($main_classes); ?>">
